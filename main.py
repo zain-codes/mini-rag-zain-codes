9150ada974457a7dbf2902d61802d60b6cb25e04
Tarek <zain20tarek@gmail.com>
2025-12-28 21:23:21 +0300

    Enhance main.py to load environment variables and update base router with API versioning and app metadata in welcome response

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI #web framework
+from dotenv import load_dotenv # order is important
+load_dotenv()
 from routes import base
 
 app = FastAPI()
--- routes/base.py
+++ routes/base.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, APIRouter
+import os
 
-base_router = APIRouter() #APIRouter is a class that helps to group related routes together
 
+#APIRouter is a class that helps to group related routes together
+base_router = APIRouter( 
+    prefix = "/api/v1",
+    tags = ["api_v1"]
+)
 
+'''
+# async is used to make the function asynchronous, it helps uvicorn to run the function in a separate thread which is 
+more efficient when there are requests coming from multiple functions or many requests at the same time
+'''
 @base_router.get("/")
-def welcome():
+async ``def welcome(): 
+    app_name = os.getenv('APP_NAME')
+    app_version = os.getenv('APP_VERSION')
+
     return {
-        "message": "Hello World"
+        "app_name": app_name,
+        "app_version": app_version,
     }
